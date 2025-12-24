19ee37faceb6cf1edbb7275d0c7eb93f00afb360
Ron-ron Aspe Rivera <ronaspe42@gmail.com>
2025-12-24 20:36:03 +0800

    testing backend

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -2,5 +2,5 @@ import "dotenv/config"
 
 export const ENV={
     PORT: process.env.PORT,
-    DB_URL: process.env.DB_URL
+    DB_URL: process.env.DB_URL,
 }
--- backend/src/server.js
+++ backend/src/server.js
@@ -3,9 +3,10 @@ import { ENV } from "./lib/env.js";
 
 const app = express();
 
-app.get("/", (req, res) =>{
+app.get("/test", (req, res) =>{
     res.status(200).json({message: "Success!!!!!"});
 })
+
 
 app.listen(ENV.PORT, () =>{
     console.log(`App is listening on port ${ENV.PORT}`);
