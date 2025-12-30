dd4c58ce152cc1afb5c577ce758572354efcfbec
Ron-ron Aspe Rivera <ronaspe42@gmail.com>
2025-12-30 19:02:09 +0800

    added a loading icon

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,20 @@ import HomePage from './pages/HomePage'
 import ProblemsPage from './pages/ProblemsPage';
 import { useUser } from '@clerk/clerk-react';
 import { DashboardPage } from './pages/DashboardPage';
+import { Loader2 } from 'lucide-react';
 
 function App() {
 
     const {isSignedIn, isLoaded} = useUser();
    
     //git rid of flickering effect
-    if(!isLoaded) return null;
-
+    
+    if (!isLoaded)
+        return (
+            <div className="h-screen w-screen flex items-center justify-center">
+                <Loader2 className="animate-spin size-20" />
+            </div>
+        );
     return (
         <Routes>
             <Route path='/' element={!isSignedIn? <HomePage/> : <Navigate to={"/dashboard"}/>}/>
