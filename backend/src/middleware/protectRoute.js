bdf9971ade38978663c3d2bf1db1b44f2dae3570
Ron-ron Aspe Rivera <ronaspe42@gmail.com>
2026-01-03 15:35:26 +0800

    change array to function

--- backend/src/middleware/protectRoute.js
+++ backend/src/middleware/protectRoute.js
@@ -1,24 +1,22 @@
 import User from "../models/user.model.js";
 
-export const protectRoute = [
-  async (req, res, next) => {
-    try {
-      const clerkId = req.auth?.userId;
+export const protectRoute = async (req, res, next) => {
+  try {
+    const clerkId = req.auth?.userId;
 
-      if (!clerkId) return res.status(401).json({ message: "Unauthorized - invalid token" });
-
-      // find user in db by clerk ID
-      const user = await User.findOne({ clerkId });
-
-      if (!user) return res.status(404).json({ message: "User not found" });
-
-      // attach user to req
-      req.user = user;
-
-      next();
-    } catch (error) {
-      console.error("Error in protectRoute middleware", error);
-      res.status(500).json({ message: "Internal Server Error" });
+    if (!clerkId) {
+      return res.status(401).json({ message: "Unauthorized" });
     }
-  },
-];
+
+    const user = await User.findOne({ clerkId });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    console.error("Error in protectRoute middleware", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
