3164c879bdfe991e88dacd579fe66e5088f02222
Ron-ron Aspe Rivera <ronaspe42@gmail.com>
2026-01-03 15:00:35 +0800

    remove /sign-in routes on backend server

--- backend/src/middleware/protectRoute.js
+++ backend/src/middleware/protectRoute.js
@@ -1,26 +1,26 @@
 import { requireAuth } from "@clerk/express";
-import User from "../models/user.model.js";
+import User from "../models/User.js";
 
 export const protectRoute = [
-    requireAuth({signInUrl: "/sign-in"}),
-    async (req, res, next) =>{
-        try {
-            const clerkId = req.auth().userId;
-            if(!clerkId) return res.status(401).json({msg: "Unauthorized - Invalid Token"});
-            
-            // find user in db by clerkId
-            const user = await User.findOne({clerkId});
+  requireAuth(),
+  async (req, res, next) => {
+    try {
+      const clerkId = req.auth().userId;
 
-            if(!user) return res.status(404).json({msg: "User not found"});
-            
-            //attach user to request
-            req.user = user;
+      if (!clerkId) return res.status(401).json({ message: "Unauthorized - invalid token" });
 
-            next();
-                
-        } catch (error) {
-            console.error("Error in protect route middleware", error);
-            res.status(500).json({msg: "Internal server error"});
-        }
+      // find user in db by clerk ID
+      const user = await User.findOne({ clerkId });
+
+      if (!user) return res.status(404).json({ message: "User not found" });
+
+      // attach user to req
+      req.user = user;
+
+      next();
+    } catch (error) {
+      console.error("Error in protectRoute middleware", error);
+      res.status(500).json({ message: "Internal Server Error" });
     }
-]
+  },
+];
